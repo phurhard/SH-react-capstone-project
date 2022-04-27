6b0c32ab7267de815610cf38805624fdc79ead7b
phurhard <phurhardeen@gmail.com>
2022-04-27 16:01:28 +0100

    Adding react router to enable routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Services from "./components/Services"
 import About from "./components/About"
 import Contact from "./components/Contact"
 import Promotions from "./components/Promotions"
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 const App = () => {
     return (
